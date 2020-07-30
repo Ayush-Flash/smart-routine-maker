94a3200eec0782bae2a3958d61173c41158eb4c5
Ayush-Flash <Ayush-Flash>
2020-07-30 15:25:52 +0530

    Added Components
    Added Login and register components.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from './navbar.module.css';
+import { Route, Link, Switch } from 'react-router-dom';
+import LoginForm from '../LoginForm/LoginForm';
+import RegForm from '../RegForm/RegForm';
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
             <ul className={styles.navlinks}>
-                <li className={styles.navlink}>Login</li>
-                <li className={styles.navlink}>Register</li>
+                <Link to='/' className={styles.navlink}>Login</Link>
+                <Link to='/register' className={styles.navlink}>Register</Link>
             </ul>
+            <Switch>
+                <Route path='/register' exact component={RegForm} />
+                <Route path='/' component={LoginForm} />
+            </Switch>
         </nav>
     );
 }
