d0e1c1f16415ed25177368de4bff33c78b6a63d2
Ayush-Flash <Ayush-Flash>
2020-07-30 17:35:31 +0530

    Changed the project structurea bit

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import styles from './loginform.module.css'
-const loginForm = () => {
-    return ( 
-        <div className={styles.FormContainer}>
-            <form className={styles.FormBox}>
-                <h1 className={styles.FormHeader}>Login</h1>
-                <hr className={styles.FormHeaderLine}/>
-                <p><input className={styles.FormInput} type='text' id='username' placeholder='Username'/></p>
-                <p><input className={styles.FormInput} type='password' id='password' placeholder='Password'/></p>
-                <p><button className={styles.FormButton}>Login</button></p>
-            </form>
-        </div> 
-    );
+import React, { Component } from 'react';
+import styles from './loginform.module.css';
+class loginForm extends Component {
+    state = {
+        username: '',
+        password: ''
+    };
+
+    onUsernameChange = (event) => {
+        this.setState({username: event.target.value});        
+    }
+    
+    onPasswordChange = (event) => {
+        this.setState({password: event.target.value});        
+    }
+
+    onLoginClick = (event) => {
+        //do client side validation here....
+        const { username, password } = this.state; 
+        if(username.length > 0 && password.length > 0) {
+                                  
+        }        
+        console.log(this.state.username, this.state.password);
+        event.preventDefault();
+    }
+
+    render() {
+        return ( 
+            <div className={styles.FormContainer}>
+                <form className={styles.FormBox} onSubmit={this.onLoginClick}>
+                    <h1 className={styles.FormHeader}>Login</h1>
+                    <hr className={styles.FormHeaderLine}/>
+                    <p><input className={styles.FormInput} onChange={this.onUsernameChange} value={this.state.username} type='text' id='username' placeholder='Username'/></p>
+                    <p><input className={styles.FormInput} onChange={this.onPasswordChange} value={this.state.password} type='password' id='password' placeholder='Password'/></p>
+                    <p><button className={styles.FormButton} type='submit'>Login</button></p>
+                </form>
+            </div> 
+        );
+    }
 }
  
 export default loginForm;
--- src/components/RegForm/RegForm.js
+++ src/components/RegForm/RegForm.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './regform.module.css'
-const regForm = () => {
-    return ( 
-        <div className={styles.FormContainer}>
-            <form className={styles.FormBox}>
-                <h1 className={styles.FormHeader}>Register</h1>
-                <hr className={styles.FormHeaderLine}/>
-                <p><input className={styles.FormInput} type='text' id='username' placeholder='Username'/></p>
-                <p><input className={styles.FormInput} type='email' id='email' placeholder='Email'/></p>
-                <p><input className={styles.FormInput} type='password' id='password' placeholder='Password'/></p>
-                <p><button className={styles.FormButton}>Register</button></p>
-            </form>
-        </div> 
-    );
+class regForm extends Component {
+    render() {
+        return ( 
+            <div className={styles.FormContainer}>
+                <form className={styles.FormBox}>
+                    <h1 className={styles.FormHeader}>Register</h1>
+                    <hr className={styles.FormHeaderLine}/>
+                    <p><input className={styles.FormInput} type='text' id='username' placeholder='Username'/></p>
+                    <p><input className={styles.FormInput} type='email' id='email' placeholder='Email'/></p>
+                    <p><input className={styles.FormInput} type='password' id='password' placeholder='Password'/></p>
+                    <p><button className={styles.FormButton}>Register</button></p>
+                </form>
+            </div> 
+        );
+    }
 }
  
 export default regForm;
